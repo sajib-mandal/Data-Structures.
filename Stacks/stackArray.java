c2924138ad7409d06a60cfd3af712148e8ba6615
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-01 13:15:43 +0530

    Create stackArray.java

---
+++ Stacks/stackArray.java
@@ -0,0 +1,59 @@
+public class MyStack{
+    private int[] stack;
+    private int size;
+    private int capacity;
+    private int top;
+
+    public MyStack(){
+        capacity = 10;
+        stack = new int[capacity];
+        top = -1;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void push(int item) {
+        // check for overflow
+        if (top >= capacity - 1) {
+            System.out.println("Error: Can not push item. Stack Overflow");
+            return;
+        }
+
+        // insert the item and update the top
+        stack[top + 1] = item;
+        top = top + 1;
+    }
+
+    /*
+    public void push(int item){
+        if (size == capacity)
+        {
+            System.out.println("Stack overflow.");
+            return;
+        }
+        stack[top + 1] = item;
+        top = top + 1;
+    }
+
+     */
+
+    void printStack(){
+        for (int i = 0; i < capacity; i++)
+            System.out.print(stack[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        MyStack stack = new MyStack();
+
+        stack.push(22);
+        stack.push(44);
+        stack.printStack();
+    }
+}
