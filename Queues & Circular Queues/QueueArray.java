0133b84b06e08e71153b21817f34cb5c6920ea18
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-02 12:46:05 +0530

    Update QueueArray.java

--- Queues & Circular Queues/QueueArray.java
+++ Queues & Circular Queues/QueueArray.java
@@ -20,7 +20,7 @@ public class QueueArray{
             System.out.println("Queue is full.");
             return;
         }
-        rear = rear + 1;
+        rear = rear + 1; // rear++
         arr[rear] = data;
     }
 
@@ -65,3 +65,71 @@ public class QueueArray{
         System.out.println("Peek is: " + q.peek());
     }
 }
+
+
+//2nd Version Queue implementation
+
+public class QueueArray{
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int front;
+        static int rear = -1;
+
+        Queue(int size){
+            this.size = size;
+            arr = new int[size];
+        }
+
+        public static boolean isEmpty(){
+            return rear == -1;
+        }
+
+        public static void add(int data){
+            if (rear == size - 1){
+                System.out.println("Queue is full!");
+                return;
+            }
+
+
+            rear = rear + 1;    //rear++
+            arr[rear] = data;
+        }
+
+        public static int remove(){
+            if (isEmpty()){
+                System.out.println("Empty queue.");
+                return -1;
+            }
+            int front = arr[0];
+            for (int i = 0; i < rear; i++){
+                arr[i] = arr[i + 1];
+            }
+            rear = rear - 1; //rear--
+            return front;
+        }
+        public static int peek(){
+            if (isEmpty()){
+                System.out.println("Empty Queue.");
+                return -1;
+            }
+            return arr[0];
+        }
+
+
+    }
+
+    public static void main(String[] args){
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+
+        while (!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
+
