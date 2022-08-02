e70c91f49264690b837bb6239f10ed29918edd5f
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-02 12:35:16 +0530

    Update QueueArray.java

--- Queues & Circular Queues/QueueArray.java
+++ Queues & Circular Queues/QueueArray.java
@@ -1,43 +1,67 @@
-public class QueueArray {
-    private int capacity;
-    private int[] queue;
+public class QueueArray{
+    private int arr[];
+    private int size;
     private int front;
     private int rear;
 
-    public QueueArray(){
-        capacity = 10;
-        queue = new int[capacity];
+    public QueueArray(int size){
+        this.size = size;
+        arr = new int[size];
         front = -1;
         rear = -1;
     }
 
-    public void enQueue(int item){
-        if (rear >= capacity - 1){
+    public boolean isEmpty(){
+        return rear == -1;
+    }
+
+    public void enQueue(int data){
+        if (rear == size - 1){
             System.out.println("Queue is full.");
             return;
         }
-        queue[rear + 1] = item;
         rear = rear + 1;
+        arr[rear] = data;
     }
-    public void printQueue(){
-        for(int i = 0; i < capacity; i++)
-            System.out.print(queue[i] + " ");
+
+    public int deQueue(){
+        if (isEmpty()){
+            System.out.println("Empty Queue.");
+            return -1;
+        }
+        int front = arr[0];
+        for (int i = 0; i < rear; i++){
+            arr[i] = arr[i + 1];
+        }
+        rear = rear - 1;  // rear--
+        return front;
+    }
+
+    public int peek(){
+        if (isEmpty()){
+            System.out.println("Empty queue.");
+            return -1;
+        }
+        return arr[0];
+    }
+
+    public void printArray(){
+        for (int i = -1; i < rear; i++)
+            System.out.print(arr[i + 1] + " ");
         System.out.println();
     }
+
+
+
     public static void main(String[] args){
-        QueueArray ob = new QueueArray();
-        ob.enQueue(4);
-        ob.enQueue(6);
-        ob.enQueue(4);
-        ob.enQueue(6);
-        ob.enQueue(4);
-        ob.enQueue(6);
-        ob.enQueue(4);
-        ob.enQueue(6);
-        ob.enQueue(4);
-        ob.enQueue(6);
-        ob.enQueue(4);
-        ob.enQueue(6);
-        ob.printQueue();
+        QueueArray q = new QueueArray(3);
+        q.enQueue(1);
+        q.enQueue(2);
+        q.enQueue(1);
+        q.enQueue(2);
+        q.printArray();
+        System.out.println("DeQueue is: " + q.deQueue());
+        q.printArray();
+        System.out.println("Peek is: " + q.peek());
     }
 }
