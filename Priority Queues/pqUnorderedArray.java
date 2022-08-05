ea1480fd0a631cdc5c380091eb413581988a7e89
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-05 16:01:52 +0530

    Update pqUnorderedArray.java

--- Priority Queues/pqUnorderedArray.java
+++ Priority Queues/pqUnorderedArray.java
@@ -37,6 +37,20 @@ public class PriorityQueueUA {
         return item;
     }
 
+    private int peek(){
+        if (size == 0){
+            System.out.println("Queue is empty.");
+            return -1;
+        }
+        int max = 0;
+        for (int i = 1; i < size; i++){
+            if (queue[max] < queue[i]){
+                max = i;
+            }
+        }
+        return queue[max];
+    }
+
     private void printArray(){
         for (int i = 0; i < size; i++)
             System.out.print(queue[i] + " ");
@@ -45,16 +59,17 @@ public class PriorityQueueUA {
 
     public static void main(String[] args){
         PriorityQueueUA ob = new PriorityQueueUA(10);
+        //insert elements
         ob.enQueue(5);
         ob.enQueue(1);
-        ob.enQueue(7);
-        ob.enQueue(3);
-        ob.enQueue(9);
-        ob.enQueue(0);
-
+        ob.enQueue(57);
+        ob.enQueue(53);
+        ob.enQueue(5);
+        ob.enQueue(500);
+        //Operation apply.
         System.out.println("Queue is: ");
         ob.printArray();
-
+        System.out.println("Max element is: " + ob.peek());
         System.out.println("Dequeue is: " + ob.deQueue());
         ob.printArray();
     }
