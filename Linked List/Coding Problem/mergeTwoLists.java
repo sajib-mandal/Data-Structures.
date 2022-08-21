aa4f24bbda65d21277feee108993e7d5f672160d
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-21 17:27:00 +0530

    Update mergeTwoLists.java

--- Linked List/Coding Problem/mergeTwoLists.java
+++ Linked List/Coding Problem/mergeTwoLists.java
@@ -56,3 +56,80 @@ public class LinkedList {
         System.out.println();
     }
 }
+
+
+
+// 2nd version
+
+class Node {
+    String val;
+    Node next;
+
+    Node(String val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class LinkedList {
+    Node head;
+    Node tail;
+    LinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+
+    private Node zipperLists(Node head1, Node head2) {
+        Node tail = head1;
+        Node current1 = head1.next;
+        Node current2 = head2;
+        int count = 0;
+
+        while (current1 != null && current2 != null) {
+            if (count % 2 == 0) {
+                tail.next = current2;
+                current2 = current2.next;
+            } else {
+                tail.next = current1;
+                current1 = current1.next;
+            }
+            tail = tail.next;
+            count += 1;
+        }
+        if (current1 != null) tail.next = current1;
+        if (current2 != null) tail.next = current2;
+        return head1;
+    }
+
+    public static void main(String[] args) {
+        LinkedList ob = new LinkedList();
+        Node a = new Node("A");
+        Node b = new Node("B");
+        Node c = new Node("C");
+        Node d = new Node("D");
+        Node e = new Node("E");
+        Node f = new Node("F");
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+
+        Node x = new Node("X");
+        Node y = new Node("Y");
+        Node z = new Node("Z");
+
+        x.next = y;
+        y.next = z;
+
+        Node mergeList = ob.zipperLists(a, x);
+        while (mergeList != null) {
+            System.out.print(mergeList.val + " ");
+            mergeList = mergeList.next;
+        }
+        System.out.println();
+    }
+}
+
