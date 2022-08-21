f07af8a2d38eebc3a1bc5ef527cef26f3865aa05
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-21 17:51:14 +0530

    Update mergeTwoLists.java

--- Linked List/Coding Problem/mergeTwoLists.java
+++ Linked List/Coding Problem/mergeTwoLists.java
@@ -79,7 +79,7 @@ public class LinkedList {
         this.tail = null;
     }
 
-
+    // iterative
     private Node zipperLists(Node head1, Node head2) {
         Node tail = head1;
         Node current1 = head1.next;
@@ -102,6 +102,19 @@ public class LinkedList {
         return head1;
     }
 
+    // recursive
+    private Node recursiveZipperList(Node head1, Node head2) {
+        if (head1 == null && head2 == null) return null;
+        if (head1 == null) return head2;
+        if (head2 == null) return head1;
+
+        Node next1 = head1.next;
+        Node next2 = head2.next;
+        head1.next = head2;
+        head2.next = recursiveZipperList(next1, next2);
+        return head1;
+    }
+
     public static void main(String[] args) {
         LinkedList ob = new LinkedList();
         Node a = new Node("A");
@@ -110,26 +123,40 @@ public class LinkedList {
         Node d = new Node("D");
         Node e = new Node("E");
         Node f = new Node("F");
-
+   
         a.next = b;
         b.next = c;
         c.next = d;
         d.next = e;
         e.next = f;
-
+        // A -> B -> C -> D -> E -> F -> NULL
         Node x = new Node("X");
         Node y = new Node("Y");
         Node z = new Node("Z");
 
         x.next = y;
         y.next = z;
-
+        // X -> Y -> Z -> NULL
+        
+        
+        // return    A -> X -> B -> Y -> C -> Z -> D -> E -> F -> NULL
+        /*
+        // iterative
         Node mergeList = ob.zipperLists(a, x);
         while (mergeList != null) {
             System.out.print(mergeList.val + " ");
             mergeList = mergeList.next;
         }
         System.out.println();
+
+         */
+
+        // recursive
+        Node mergeList1 = ob.recursiveZipperList(a, x);
+        while (mergeList1 != null) {
+            System.out.print(mergeList1.val + " ");
+            mergeList1 = mergeList1.next;
+        }
+        System.out.println();
     }
 }
-
